f9fb89b1bd1c76277dd24540d7e1429263b33748
MethuParoi <paroimethu@gmail.com>
2024-10-22 09:56:22 +0600

    banner issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "nixzobd.com",
+        port: "",
+        pathname: "/_next/**",
+      },
+      {
+        protocol: "https",
         hostname: "kjqzojrvmhadxwftawlo.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/product_images/**",
